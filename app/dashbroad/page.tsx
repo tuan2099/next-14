1da0c7e63794e1f3a6737f4bdf8bd2f0edd7a823
tuan2099 <gwoqkepspsld@gmail.com>
2023-12-15 14:09:04 +0700

    up

---
+++ app/dashbroad/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Dashbroad() {
+  return <div>Dashbroad </div>;
+}
+
+export default Dashbroad;
---
+++ app/userList/[user]/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React from "react";
+
+function User({ params }: any) {
+  console.log(params, "đay là param động");
+  return <div>User</div>;
+}
+
+export default User;
